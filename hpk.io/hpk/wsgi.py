364fa0cd23c706d586859de3a8c7a6e27020b086
Ada Wright <ada@hpk.io>
2020-09-20 11:16:36 +0800

    Update namespace to hpk

--- hpk.io/hpk/wsgi.py
+++ hpk.io/hpk/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djpol project.
+WSGI config for hpk project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djpol.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hpk.settings')
 
 application = get_wsgi_application()
--- hpk.io/manage.py
+++ hpk.io/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djpol.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hpk.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
